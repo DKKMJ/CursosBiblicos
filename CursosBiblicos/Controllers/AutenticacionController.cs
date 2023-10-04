07e43ea7aa21dde4f34476928a212560d0aff4df
Katherine Patino <kpatino22@cftsa.cl>
2023-10-04 18:16:24 -0300

    Controlador de Autenticacion agregado

---
+++ CursosBiblicos/Controllers/AutenticacionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CursosBiblicos.DTOS;
+
+namespace CursosBiblicos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutenticacionController : ControllerBase
+    {
+
+        [HttpPost("IniciarSesion")]
+        public IActionResult IniciarSesion([FromBody] AutenticacionDTO autenticacionDTO)
+        {
+            try
+            {
+                // Simplemente devuelve un mensaje de inicio de sesión exitoso.
+                return Ok(new { Message = "Inicio de sesión exitoso" });
+            }
+            catch (Exception ex)
+            {
+                // En caso de error, devuelve un mensaje de error interno del servidor.
+                return StatusCode(500, new { Message = "Ocurrió un error al iniciar sesión", Error = ex.Message });
+            }
+        }
+    }
+}
---
+++ CursosBiblicos/DTOS/AutenticacionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CursosBiblicos.DTOS
+{
+    public class AutenticacionDTO
+    {
+        public string NombreDeUsuario { get; set; }
+        public string Contrasena { get; set; }
+        //public string? Permisos { get; set; }
+    }
+}
